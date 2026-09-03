2aef66cab87f42b8e6eee5f9b1bd95496a1caa03
sistemaslogictel <sistemaslogictel@gmail.com>
2026-09-03 10:19:11 -0300

    23.21

--- js/utils.js
+++ js/utils.js
@@ -79,7 +79,7 @@ export function registrarUltimaAtualizacao() {
     const agora = new Date();
     const dia = String(agora.getDate()).padStart(2, '0');
     const mes = String(agora.getMonth() + 1).padStart(2, '0');
-    const ano = String(agora.getFullYear()).slice(2);
+    const ano = String(agora.getFullYear()); // CORRIGIDO: ano com 4 dígitos
     const horas = String(agora.getHours()).padStart(2, '0');
     const minutos = String(agora.getMinutes()).padStart(2, '0');
     const dataFormatada = `${dia}/${mes}/${ano}, ${horas}:${minutos}`;
